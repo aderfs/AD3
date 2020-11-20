3dc6d13b7cc21b17f315387f6cdc5ea7cfa8c43e
aderfs <74571192+aderfs@users.noreply.github.com>
2020-11-20 20:48:33 -0300

    Update Vaga.java

--- src/Model/Vaga.java
+++ src/Model/Vaga.java
@@ -3,6 +3,7 @@ package Model;
 
 public class Vaga extends Empresa {
     
+    private int ID_Vaga;
     private String descricaovaga;
     private String prerequisito;
     private String especificacoes;
@@ -11,19 +12,29 @@ public class Vaga extends Empresa {
     public Vaga() {
     }
 
-    public Vaga(String descricaovaga, String prerequisito, String especificacoes, String contato) {
+    public Vaga(int ID_Vaga, String descricaovaga, String prerequisito, String especificacoes, String contato) {
+        this.ID_Vaga = ID_Vaga;
         this.descricaovaga = descricaovaga;
         this.prerequisito = prerequisito;
         this.especificacoes = especificacoes;
         this.contato = contato;
     }
 
-    public Vaga(String descricaovaga, String prerequisito, String especificacoes, String contato, int IDempresa, String CNPJ, String CEP, String endereco, String nome) {
+    public Vaga(int ID_Vaga,String descricaovaga, String prerequisito, String especificacoes, String contato, int IDempresa, String CNPJ, String CEP, String endereco, String nome) {
         super(IDempresa, CNPJ, CEP, endereco, nome);
+        this.ID_Vaga = ID_Vaga;
         this.descricaovaga = descricaovaga;
         this.prerequisito = prerequisito;
         this.especificacoes = especificacoes;
         this.contato = contato;
+    }
+    
+    public String getID_Vaga() {
+        return ID_Vaga;
+    }
+    
+    public void ID_Vaga(int ID_Vaga) {
+        this.ID_Vaga = ID_Vaga;
     }
 
     public String getDescricaovaga() {
@@ -58,7 +69,13 @@ public class Vaga extends Empresa {
         this.contato = contato;
     }
     
+    public boolean Cadastro_Vaga() {
+        return true; //adicionar o codigo o cadastro de uma vaga
+    }
     
+    public boolean Gerenciar_Vaga() {
+        return true; //adicionar o codigo o gerenciamento de uma vaga
+    }
     
     
     
