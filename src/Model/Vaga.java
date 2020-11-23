c5a8c1106564e1ea5deeffe36855b04320d00753
Arthur-alg <74572897+Arthur-alg@users.noreply.github.com>
2020-11-23 20:31:32 -0300

    Update Vaga.java

--- src/Model/Vaga.java
+++ src/Model/Vaga.java
@@ -1,81 +1,94 @@
+
 package Model;
+import java.util.*;
+import java.sql.*;
 
 
 public class Vaga {
     
-    private int ID_Vaga;
-    private String descricaovaga;
-    private String prerequisito;
-    private String especificacoes;
-    private String contato;
+    public static int ID_vaga;
+    private String Descricaovaga;
+    private String Prerequisito;
+    private String Especificacoes;
+    private String Contato;
 
     public Vaga() {
     }
 
-    public Vaga(int ID_Vaga, String descricaovaga, String prerequisito, String especificacoes, String contato) {
-        this.ID_Vaga = ID_Vaga;
-        this.descricaovaga = descricaovaga;
-        this.prerequisito = prerequisito;
-        this.especificacoes = especificacoes;
-        this.contato = contato;
+    public Vaga(int ID_vaga, String Descricaovaga, String Prerequisito, String Especificacoes, String Contato) {
+        this.ID_vaga = ID_vaga;
+        this.Descricaovaga = Descricaovaga;
+        this.Prerequisito = Prerequisito;
+        this.Especificacoes = Especificacoes;
+        this.Contato = Contato;
     }
 
-    public Vaga(int ID_Vaga,String descricaovaga, String prerequisito, String especificacoes, String contato, int IDempresa, String CNPJ, String CEP, String endereco, String nome) {
-        super(IDempresa, CNPJ, CEP, endereco, nome);
-        this.ID_Vaga = ID_Vaga;
-        this.descricaovaga = descricaovaga;
-        this.prerequisito = prerequisito;
-        this.especificacoes = especificacoes;
-        this.contato = contato;
+    public int getID_vaga() {
+        return ID_vaga;
     }
-    
-    public String getID_Vaga() {
-        return ID_Vaga;
-    }
-    
-    public void ID_Vaga(int ID_Vaga) {
-        this.ID_Vaga = ID_Vaga;
+
+    public void setID_vaga(int ID_vaga) {
+        this.ID_vaga = ID_vaga;
     }
 
     public String getDescricaovaga() {
-        return descricaovaga;
+        return Descricaovaga;
     }
 
-    public void setDescricaovaga(String descricaovaga) {
-        this.descricaovaga = descricaovaga;
+    public void setDescricaovaga(String Descricaovaga) {
+        this.Descricaovaga = Descricaovaga;
     }
 
     public String getPrerequisito() {
-        return prerequisito;
+        return Prerequisito;
     }
 
-    public void setPrerequisito(String prerequisito) {
-        this.prerequisito = prerequisito;
+    public void setPrerequisito(String Prerequisito) {
+        this.Prerequisito = Prerequisito;
     }
 
     public String getEspecificacoes() {
-        return especificacoes;
+        return Especificacoes;
     }
 
-    public void setEspecificacoes(String especificacoes) {
-        this.especificacoes = especificacoes;
+    public void setEspecificacoes(String Especificacoes) {
+        this.Especificacoes = Especificacoes;
     }
 
     public String getContato() {
-        return contato;
+        return Contato;
     }
 
-    public void setContato(String contato) {
-        this.contato = contato;
+    public void setContato(String Contato) {
+        this.Contato = Contato;
     }
-    
-    public boolean Cadastro_Vaga() {
-        //informar o id da empresa dona da vaga
-        return true; //adicionar o codigo o cadastro de uma vaga
+
+     public ArrayList getListavagas() {
+        return dao.getListavagas();
     }
-    
-    public boolean Gerenciar_Vaga() {
-        return true; //adicionar o codigo o gerenciamento de uma vaga
+
+    public boolean InserirVagas(Vagas objeto) {
+        dao.InserirVagas(objeto);
+        return true;
+    }
+
+    public boolean DeletarVagas(int ID_vagas) {
+        dao.DeletarVagas(ID_vagas);
+        return true;
+    }
+
+    public boolean AtualizarVagas(Vagas objeto) {
+        dao.AtualizarVagas(objeto);
+        return true;
+    }
+
+    public Curriculo dadosVagas(int ID_vagas) {
+        dao.dadosVagas(ID_vagas);
+        return null;
+    }
+
+    public int maiorID_vagas() throws SQLException {
+        return dao.maiorID_vagas();
     }
     
     
