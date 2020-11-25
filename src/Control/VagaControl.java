0c494000eb4f4395efc91ce9e11be43a8b18cfdc
aderfs <74571192+aderfs@users.noreply.github.com>
2020-11-25 19:47:11 -0300

    Update VagaControl.java

--- src/Control/VagaControl.java
+++ src/Control/VagaControl.java
@@ -6,21 +6,21 @@ import java.sql.*;
 
 public class VagaControl {
    
-   private final Vaga control; 
+   private final Vaga vaga; 
    //atributo para facilitar mudancas no nome do model
    
    public VagaControl() {
-       this.control = new Vaga();
+       this.vaga = new Vaga();
        //contrutor que facilita mudancas na camada model sem afetar tanto o control
    }
    
    //create
-   public boolean CastrarVaga(String Descricaovaga, String Prerequisito, String Especificacoes, String Contato) {
+   public boolean CastrarVaga(String Descricaovaga, String Prerequisito, String Especificacoes, String Contato)throws SQLException {
       
-      int ID_vaga = control.maiorID_Vaga() + 1; //atribui um novo maior id para o objeto ainda sem id
-      Vaga objeto = new Vaga(ID_vaga, DescricaoVaga, Prerequisito, Especificacoes, Contato);
+      int id = 0; //BD possui auto increment
+      Vaga objeto = new Vaga(id, Descricaovaga, Prerequisito, Especificacoes, Contato);
       
-      if(control.InserirVagas(objeto)){
+      if(vaga.InserirVagas(objeto)){
          return true;
       }else{
          return false;
@@ -28,9 +28,9 @@ public class VagaControl {
    }
    
    //Update
-   public boolean EditarVaga(int ID_vaga, String Descricaovaga, String Prerequisito, String Especificacoes, String Contato) {
-      Vaga objeto = new Vaga(ID_vaga, DescricaoVaga, Prerequisito, Especificacoes, Contato);
-      if(control.AtualizarVagas(ID_vaga, objeto){
+   public boolean EditarVaga(int id, String Descricaovaga, String Prerequisito, String Especificacoes, String Contato) {
+      Vaga objeto = new Vaga(id, Descricaovaga, Prerequisito, Especificacoes, Contato);
+      if(vaga.AtualizarVagas(id, objeto)){
         return true;
       }else{
         return false;
@@ -38,8 +38,8 @@ public class VagaControl {
    }
    
    //Delete
-   public boolean ApagarVaga(ind ID_vaga){
-      if(control.DeletarVagas(ID_vaga){
+   public boolean ApagarVaga(int id){
+      if(vaga.DeletarVagas(id)){
         return true;
       }else{
         return false;
@@ -47,8 +47,8 @@ public class VagaControl {
     }
     
    //Read
-   public ArrayList getListaVagas()
-      return control.getListaVagas();
+   public ArrayList getListaVagas() {
+       return vaga.getListavagas();
    }
    
    
