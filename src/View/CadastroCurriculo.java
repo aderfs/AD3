a409b3d576aaf3718d7497d4acfbeb8bff8d1769
dmrdreams <74572867+dmrdreams@users.noreply.github.com>
2020-11-25 22:21:55 -0300

    Add files via upload

--- src/View/CadastroCurriculo.java
+++ src/View/CadastroCurriculo.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
-/**
- *
- * @author danie
- */
+import Control.CurriculoControl;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class CadastroCurriculo extends javax.swing.JFrame {
 
-    /**
-     * Creates new form CadastroCandidato
-     */
+    private CurriculoControl Controlador;
+
     public CadastroCurriculo() {
         initComponents();
+        this.Controlador = new CurriculoControl();
     }
 
     /**
@@ -28,15 +25,15 @@ public class CadastroCurriculo extends javax.swing.JFrame {
     private void initComponents() {
 
         L_Nome_Curriculo = new javax.swing.JLabel();
-        T_Nome_Curriculo = new javax.swing.JTextField();
+        C_Nome_Curriculo = new javax.swing.JTextField();
         L_Idade_Curriculo = new javax.swing.JLabel();
-        T_idade_Curriculo = new javax.swing.JTextField();
+        C_Idade_Curriculo = new javax.swing.JTextField();
         L_CPF_Curriculo = new javax.swing.JLabel();
-        T_CPF_Curriculo = new javax.swing.JTextField();
+        C_CPF_Curriculo = new javax.swing.JTextField();
         L_RG_Curriculo = new javax.swing.JLabel();
-        T_RG_Curriculo = new javax.swing.JTextField();
+        C_RG_Curriculo = new javax.swing.JTextField();
         L_Curriculo = new javax.swing.JLabel();
-        T_Curriculo = new javax.swing.JTextField();
+        C_Curriculo = new javax.swing.JTextField();
         B_Cancelar_Curriculo = new javax.swing.JButton();
         B_Cadastrar_Curriculo = new javax.swing.JButton();
 
@@ -44,17 +41,17 @@ public class CadastroCurriculo extends javax.swing.JFrame {
 
         L_Nome_Curriculo.setText("Nome:");
 
-        T_Nome_Curriculo.addActionListener(new java.awt.event.ActionListener() {
+        C_Nome_Curriculo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                T_Nome_CurriculoActionPerformed(evt);
+                C_Nome_CurriculoActionPerformed(evt);
             }
         });
 
         L_Idade_Curriculo.setText("Idade:");
 
-        T_idade_Curriculo.addActionListener(new java.awt.event.ActionListener() {
+        C_Idade_Curriculo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                T_idade_CurriculoActionPerformed(evt);
+                C_Idade_CurriculoActionPerformed(evt);
             }
         });
 
@@ -64,10 +61,10 @@ public class CadastroCurriculo extends javax.swing.JFrame {
 
         L_Curriculo.setText("Curriculo:");
 
-        T_Curriculo.setToolTipText("");
-        T_Curriculo.addActionListener(new java.awt.event.ActionListener() {
+        C_Curriculo.setToolTipText("");
+        C_Curriculo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                T_CurriculoActionPerformed(evt);
+                C_CurriculoActionPerformed(evt);
             }
         });
 
@@ -79,6 +76,11 @@ public class CadastroCurriculo extends javax.swing.JFrame {
         });
 
         B_Cadastrar_Curriculo.setText("Cadastrar");
+        B_Cadastrar_Curriculo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                B_Cadastrar_CurriculoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -91,11 +93,11 @@ public class CadastroCurriculo extends javax.swing.JFrame {
                         .addComponent(B_Cancelar_Curriculo)
                         .addGap(100, 100, 100)
                         .addComponent(B_Cadastrar_Curriculo))
-                    .addComponent(T_RG_Curriculo)
-                    .addComponent(T_CPF_Curriculo)
-                    .addComponent(T_Nome_Curriculo)
-                    .addComponent(T_Curriculo)
-                    .addComponent(T_idade_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(C_RG_Curriculo)
+                    .addComponent(C_CPF_Curriculo)
+                    .addComponent(C_Nome_Curriculo)
+                    .addComponent(C_Curriculo)
+                    .addComponent(C_Idade_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(L_Idade_Curriculo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(L_CPF_Curriculo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(L_RG_Curriculo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -109,23 +111,23 @@ public class CadastroCurriculo extends javax.swing.JFrame {
                 .addGap(35, 35, 35)
                 .addComponent(L_Nome_Curriculo)
                 .addGap(20, 20, 20)
-                .addComponent(T_Nome_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(C_Nome_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25)
                 .addComponent(L_Idade_Curriculo)
                 .addGap(20, 20, 20)
-                .addComponent(T_idade_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(C_Idade_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25)
                 .addComponent(L_CPF_Curriculo)
                 .addGap(20, 20, 20)
-                .addComponent(T_CPF_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(C_CPF_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25)
                 .addComponent(L_RG_Curriculo)
                 .addGap(20, 20, 20)
-                .addComponent(T_RG_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(C_RG_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25)
                 .addComponent(L_Curriculo)
                 .addGap(20, 20, 20)
-                .addComponent(T_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(C_Curriculo, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(B_Cancelar_Curriculo)
@@ -137,21 +139,77 @@ public class CadastroCurriculo extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void T_Nome_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_T_Nome_CurriculoActionPerformed
+    private void C_Nome_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_C_Nome_CurriculoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_T_Nome_CurriculoActionPerformed
+    }//GEN-LAST:event_C_Nome_CurriculoActionPerformed
 
-    private void T_idade_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_T_idade_CurriculoActionPerformed
+    private void C_Idade_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_C_Idade_CurriculoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_T_idade_CurriculoActionPerformed
+    }//GEN-LAST:event_C_Idade_CurriculoActionPerformed
 
     private void B_Cancelar_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_B_Cancelar_CurriculoActionPerformed
-         this.setVisible(false);
+        this.setVisible(false);
     }//GEN-LAST:event_B_Cancelar_CurriculoActionPerformed
 
-    private void T_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_T_CurriculoActionPerformed
+    private void C_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_C_CurriculoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_T_CurriculoActionPerformed
+    }//GEN-LAST:event_C_CurriculoActionPerformed
+
+    private void B_Cadastrar_CurriculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_B_Cadastrar_CurriculoActionPerformed
+        try {
+            // recebendo e validando dados da interface gráfica.
+            String Nome = "";
+            int Idade = 0;
+            String CPF = "";
+            String RG = "";
+            String Curriculo = "";
+
+            if (this.C_Nome_Curriculo.getText().length() < 2) {
+                throw new Mensagens("Nome deve conter ao menos 2 caracteres.");
+            } else {
+                Nome = this.C_Nome_Curriculo.getText();
+            }
+            if (this.C_Idade_Curriculo.getText().length() < 0) {
+                throw new Mensagens("Idade deve conter ao menos 1 caracterer.");
+            } else {
+                Idade = Integer.parseInt(this.C_Idade_Curriculo.getText());
+            }
+            if (this.C_CPF_Curriculo.getText().length() < 2) {
+                throw new Mensagens("CPF deve conter ao menos 2 caracteres.");
+            } else {
+                CPF = this.C_CPF_Curriculo.getText();
+            }
+            if (this.C_RG_Curriculo.getText().length() < 2) {
+                throw new Mensagens("RG deve conter ao menos 2 caracteres.");
+            } else {
+                RG = this.C_RG_Curriculo.getText();
+            }
+            if (this.C_Curriculo.getText().length() < 2) {
+                throw new Mensagens("Curriculo deve conter ao menos 2 caracteres.");
+            } else {
+                Curriculo = this.C_Curriculo.getText();
+            }
+            // envia os dados para o Controlador cadastrar
+            if (this.Controlador.CadastrarCurriculo(Nome, Curriculo, CPF, RG, Idade)) {
+                JOptionPane.showMessageDialog(rootPane, "Aluno Cadastrado com Sucesso!");
+
+                // limpa campos da interface
+                this.C_Nome_Curriculo.setText("");
+                this.C_Idade_Curriculo.setText("");
+                this.C_CPF_Curriculo.setText("");
+                this.C_RG_Curriculo.setText("");
+                this.C_Curriculo.setText("");
+            }
+            System.out.println(this.Controlador.getListacurriculos().toString());
+
+        } catch (Mensagens erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } catch (NumberFormatException erro2) {
+            JOptionPane.showMessageDialog(null, "Informe um número.");
+        } catch (SQLException ex) {
+            Logger.getLogger(CadastroCurriculo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_B_Cadastrar_CurriculoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -192,15 +250,15 @@ public class CadastroCurriculo extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton B_Cadastrar_Curriculo;
     private javax.swing.JButton B_Cancelar_Curriculo;
+    private javax.swing.JTextField C_CPF_Curriculo;
+    private javax.swing.JTextField C_Curriculo;
+    private javax.swing.JTextField C_Idade_Curriculo;
+    private javax.swing.JTextField C_Nome_Curriculo;
+    private javax.swing.JTextField C_RG_Curriculo;
     private javax.swing.JLabel L_CPF_Curriculo;
     private javax.swing.JLabel L_Curriculo;
     private javax.swing.JLabel L_Idade_Curriculo;
     private javax.swing.JLabel L_Nome_Curriculo;
     private javax.swing.JLabel L_RG_Curriculo;
-    private javax.swing.JTextField T_CPF_Curriculo;
-    private javax.swing.JTextField T_Curriculo;
-    private javax.swing.JTextField T_Nome_Curriculo;
-    private javax.swing.JTextField T_RG_Curriculo;
-    private javax.swing.JTextField T_idade_Curriculo;
     // End of variables declaration//GEN-END:variables
 }
